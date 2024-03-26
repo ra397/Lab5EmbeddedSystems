d7581250133461648a540dab9a656fed70f7f784
ra397 <rabialaya@gmail.com>
2024-03-26 12:30:47 -0500

    first commit

---
+++ Lab5/Lab5/main.c
@@ -0,0 +1,20 @@
+/*
+ * Lab5.c
+ *
+ * Created: 3/26/2024 12:24:30 PM
+ * Author : Rabi Alaya and Alex Banning
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		printf("Hello World");
+    }
+}
+
