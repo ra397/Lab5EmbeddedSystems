24aff2070fb77bc30dff05610dbe8c9f7e0bb5a1
ra397 <rabialaya@gmail.com>
2024-04-01 21:01:29 -0500

    got RPG functionality, RPG changes Duty Cycle changing the fan speed, the change is displayed to LCD

--- Lab5/Lab5/main.c
+++ Lab5/Lab5/main.c
@@ -29,6 +29,8 @@ FILE lcd_str = FDEV_SETUP_STREAM(lcd_putchar, NULL, _FDEV_SETUP_WRITE);
 volatile uint8_t COMPARE = 100;
 
 void timer0_init();
+void read_rpg();
+void display_to_LCD();
 void doubleToString(double double_value, char* str_freq, size_t max_len);
 
 
@@ -40,29 +42,25 @@ ISR(TIMER0_OVF_vect)
 
 int main(void)
 {
-	stdout = &lcd_str; // redefines std output to output to LCD file output, that file is what writes to LCD
+	stdout = &lcd_str; // redefines std output to output to LCD file output, that file is that writes to LCD
 	clock_prescale_set(1);
 
 	lcd_init();
-	printf("Hello TA:");
-	row2();
 	
 	DDRD = DDRD | (1 << 5); // set PD5 as output
-	DDRB = DDRB & ~((1 << RPG_A_PIN) | (1 << RPG_B_PIN));
+	DDRB = DDRB & ~((1 << RPG_A_PIN) | (1 << RPG_B_PIN)); // Set RPG pins as inputs
+	PORTB |= (1 << RPG_A_PIN) | (1 << RPG_B_PIN);
 	
 	
 	timer0_init();
 	
-	double dutyCycle = (double) COMPARE / TOP;
-	char str_freq[20];
-	doubleToString(dutyCycle, str_freq, sizeof(str_freq));
-	printf("Duty Cycle: %s", str_freq);
-	
+	printf("Hello TA:");
 	
     while (1) {
-
+		display_to_LCD();
+		read_rpg();
+		_delay_ms(10);
     }
-	
 }
 
 /*
@@ -106,3 +104,34 @@ void doubleToString(double double_value, char *str_freq, size_t max_len) {
 	snprintf(str_freq, max_len, "%d.%02d", intPart, fracPart);
 }
 
+/*
+* A function that reads the state of the RPG and updates COMPARE accordingly6
+*/
+void read_rpg() {
+	static uint8_t old_state = 0;
+	uint8_t new_state = RPG_PORT & ((1 << RPG_A_PIN) | (1 << RPG_B_PIN));
+
+	if (new_state == old_state) return; // No change
+
+	// Determine direction based on state changes
+	if (((old_state == 0x00) && (new_state == 0x01)) ||
+	((old_state == 0x01) && (new_state == 0x03)) ||
+	((old_state == 0x03) && (new_state == 0x02)) ||
+	((old_state == 0x02) && (new_state == 0x00))) {
+		// Clockwise rotation
+		COMPARE--;
+		} else {
+		// Counterclockwise rotation
+		COMPARE++;
+	}
+
+	old_state = new_state;
+}
+
+void display_to_LCD() {
+	row2();
+	double dutyCycle = (double) COMPARE / TOP;
+	char str_freq[20];
+	doubleToString(dutyCycle, str_freq, sizeof(str_freq));
+	printf("Duty Cycle: %s", str_freq);
+}
